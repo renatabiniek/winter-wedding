7acbda35da64d25b0cdefb57d9234bf9d2adc31a
renatabiniek <renata.biniek@gmail.com>
2022-01-08 17:02:44 +0000

    Add validation function to check email syntax

--- run.py
+++ run.py
@@ -10,6 +10,10 @@ import gspread
 # To set up authentication with creds.json to access the project on Google Cloud
 from google.oauth2.service_account import Credentials
 
+# Import re module to validate email address
+
+import re
+
 # Used from CI Love Sandwiches walkthrough project to access data on the spreadsheet
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -29,7 +33,25 @@ def get_guest_info():
     print("What's your email address?\n")
 
     email_str = input("Enter email address:\n")
-    print(f"You entered {email_str}")
-    print("Checking email addres...")
+    print("Checking your email address...\n")
+    validate_email(email_str)
+
+
+# From https://stackabuse.com/python-validate-email-address-with-regular-expressions-regex/
+
+def validate_email(email):
+    """
+    Using Regular Expression, validates basic syntax of email address
+    """
+    # Regular expression for validating the email
+    regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+
+    try:
+        if (not re.fullmatch(regex, email)):
+            raise ValueError(
+                f"Email {email} seems incorrect"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please enter your email address again.\n")
 
 get_guest_info()
