f56bcbe8c62f9c4605069a052a4881ea70804deb
renatabiniek <renata.biniek@gmail.com>
2022-01-09 15:00:44 +0000

    Change update spreadsheet functions using an empty list and append_row

--- run.py
+++ run.py
@@ -57,7 +57,12 @@ You've been invited to our wedding!\n
 Please RSVP here.\n
     ''')
 
+
 print_logo()
+
+# Empty list to collect responses from terminal and
+# later append to the main worksheet 
+rsvp_info = []
 
 def get_guest_info():
     """
@@ -68,7 +73,8 @@ def get_guest_info():
     email_str = input("Enter email address:\n").lower()
     print("Checking your email address...\n")
     validate_email(email_str)
-    
+    rsvp_info.append(email_str)
+
     return email_str
 
 
@@ -90,15 +96,19 @@ def validate_email(email):
         print(f"Invalid data: {e}, please enter your email address again.\n")
 
 
-def add_guest(email):
+def add_guest(data):
     """
     Identify first empty row on main worksheet and add guest email
     """
-    print("Adding guest email to main worksheet...\n")
+    print("Adding guest email and timestamp to main worksheet...\n")
     main_worksheet = SHEET.worksheet("main")
-    first_empty_row = (len(main_worksheet.get_all_values()) + 1)
-    main_worksheet.update_cell(first_empty_row, 2, email)
-    print("Email added to main worksheet...!")
+
+    # first_empty_row = (len(main_worksheet.get_all_values()) + 1)
+    # main_worksheet.update_cell(first_empty_row, 2, email)
+    get_timestamp()    
+    main_worksheet.append_row(data)
+
+    print("Email and stamp added to main worksheet...!")
 
 def get_timestamp():
     """
@@ -106,10 +116,23 @@ def get_timestamp():
     """
     now = datetime.now()
     # Format the date and time as dd/mm/YY H:M:S
-    now = now.strftime("%d/%m/%Y %H:%M:%S")
-    return now
+    stamp = now.strftime("%d/%m/%Y %H:%M:%S")
+    rsvp_info.append(stamp)
+    return stamp
+
+# def add_timestamp(date):
+#     """
+#     Identify first empty row on main worksheet and add guest email
+#     """
+#     print("Adding date of submission to main worksheet...\n")
+#     main_worksheet = SHEET.worksheet("main")
+#     first_empty_row = (len(main_worksheet.get_all_values()) + 1)
+#     main_worksheet.update_cell(first_empty_row, 1, date)
+#     print("Timestamp added to main worksheet...!")
 
 
 guest_email = get_guest_info()
-add_guest(guest_email)
+add_guest(rsvp_info)
 submission_date = get_timestamp()
+# add_timestamp(submission_date)
+print(rsvp_info)
